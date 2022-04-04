50ca4c0b9b3b71a09a4a0450dc6d8b3ce70812e5
Dmitry <ggbox@mail.ru>
2022-04-04 18:21:30 +0400

    path fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/public/',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
     filename: 'bundle.js',
   },
   devServer: {
